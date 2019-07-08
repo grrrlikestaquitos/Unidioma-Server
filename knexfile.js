2208ddd60ac8180c51774148edce047ce4c68810
Andrei <andreicrimson@gmail.com>
2019-07-07 22:02:50 -0700

    specifying prod connection and migrations

--- knexfile.js
+++ knexfile.js
@@ -6,35 +6,15 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: __dirname + 'migrations'
     }
   }
 
