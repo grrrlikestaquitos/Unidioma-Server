227c01e8d5d1f773b898576bc618bcd5e5c581e0
Andrei <andreicrimson@gmail.com>
2020-01-04 10:52:20 -0800

    algorithm for updating character id for a given language

--- src/logic/language-character-algo.ts
+++ src/logic/language-character-algo.ts
@@ -4,13 +4,13 @@ import * as moment from 'moment'
 
 type Config = { [key in LanguageType]: CharacterType }
 
+// TODO: Create a single source of truth for this config, currently there are multiple
 const LanguageConfig: Config = {
     japanese: CharacterType.kanji,
     chinese: CharacterType.pinyin
 }
 
 export const determineCharacterForTheDay = () => {
-    
     Object.entries(LanguageConfig).map(async ([language, selectedType]) => {
         const knex = database(language)
         const typeInfoArray = await knex.where({ type: selectedType }).select('type', 'id', 'last_updated')
@@ -20,10 +20,11 @@ export const determineCharacterForTheDay = () => {
             const didExpire = checkIfTimestampExpired(last_updated)
 
             if (didExpire) {
-                const resetTimestamp = moment().startOf('day').add(5, 'hours').format(DateFormat.detailed)
-                knex.where({ type }).update({ last_updated: resetTimestamp })
-            }
+                const resetTimestamp = getResetTimestamp()
+                const updatedId = await getUpdatedIdForType(type, id)
 
+                await knex.where({ type }).update({ id: updatedId, last_updated: resetTimestamp })
+            }
         })
     })
 }
@@ -34,4 +35,24 @@ const checkIfTimestampExpired = (timestamp: string) => {
     const differenceGreaterThan24Hours = differenceInHours >= 24
 
     return differenceGreaterThan24Hours
+}
+
+const getResetTimestamp = () => {
+    return moment().startOf('day').add(5, 'hours').format(DateFormat.detailed)
+}
+
+const getUpdatedIdForType = async (type: CharacterType, oldId: number) => {
+    const knexType = database(type)
+    const count: number = (await knexType.count())[0]['count'] as number - 1
+
+    if (count > oldId) { // There are still more characters to go through, update to next character
+        const updatedId = oldId + 1
+
+        console.log(`Id was updated ${updatedId}`)
+        return updatedId
+    }
+
+    if (oldId === count) { // We've reached the end of characters, more characters need to be added
+        console.log(`Id is identical, end met`)   
+    } 
 }
